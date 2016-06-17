891128534d5cdc4edf681c6ddc413327b9a23207
Kaspars Bulins <kasparsb@hotmail.com>
2016-06-17 15:54:24 +0300

    Example

--- example/app.js
+++ example/app.js
@@ -1,1 +1,21 @@
-var s = new webit.Slider();
+var $ = jQuery;
+
+var slider = $('.slider').get(0);
+var items = [];
+$('.slider .item').each(function(){
+    items.push(this);
+})
+
+var s = new webit.Slider(
+    slider,
+    items
+);
+
+$(document).on('click', 'button', function(){
+    if ($(this).hasClass('prev')) {
+        s.prev();
+    }
+    else if ($(this).hasClass('next')) {
+        s.next();
+    }
+})
